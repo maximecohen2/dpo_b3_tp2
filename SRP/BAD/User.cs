5ef2f71b435a8676626e7b727fdcae568ea04002
fortinmeihdi <fortinmeihdi@gmail.com>
2018-03-08 20:58:24 +0100

    push 2

---
+++ SRP/BAD/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRP
+{
+    class User
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public Guid UserId { get; set; }
+
+        public void Insert()
+        { }
+
+        public void Update()
+        { }
+
+
+        public bool IsValid()
+        {
+            bool isValid=false;
+
+
+            return isValid;
+        }
+
+
+        public int GetAge()
+        {
+            int age = 0;
+
+
+            return age;
+        }
+
+
+
+
+    }
+}
